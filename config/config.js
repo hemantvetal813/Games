ed6a4c815b2bb25adf80700bcd3f339492828cf2
Hemant vetal <48785645+hemantvetal813@users.noreply.github.com>
2020-10-31 15:04:26 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,15 +1,10 @@
 let environment = {
     development: {
-        //        "db_host": "vernostdevuat.cz7vnhsdptrh.us-east-1.rds.amazonaws.com", //
-        // "db_username": "bespoke",
-        // "db_password": "b3$p0k3",
-        // "db_name": "BeSpokesitdb",
-        // "jwt_token_secret": "bespoke",
         "db_host": "localhost",
         "db_username": "root",
         "db_password": "Nopass!111",
-        "db_name": "Tenant_Master",
-        "jwt_token_secret": "bespoke",
+        "db_name": "jkasdnjksd",
+        "jwt_token_secret": "bdaksbdjak",
         port: 3000,
         expireTime: 60 * 60 * 24,
         }
